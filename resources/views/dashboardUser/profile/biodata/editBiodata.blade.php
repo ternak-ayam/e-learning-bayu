47ef40c097dfa42bd0f0b8440bbd1fe70a05f083
prasta <prasta72@gmail.com>
2023-07-12 16:46:26 +0800

    1

--- resources/views/dashboardUser/profile/biodata/editBiodata.blade.php
+++ resources/views/dashboardUser/profile/biodata/editBiodata.blade.php
@@ -17,10 +17,10 @@
                              {{$user->email}}</h5>
                         </h5>
                         <p class="font-normal text-gray-700 dark:text-gray-400">Telepon</p>
-                        <h5 class="mb-10 text-md font-semibold tracking-tight text-gray-900 dark:text-white"> {{'0' . $user->phone}}</h5>
+                        <h5 class="mb-10 text-md font-semibold tracking-tight text-gray-900 dark:text-white"> {{$user->phone ? '0' . $user->phone : '-'}}</h5>
                         </h5>
                         <p class="font-normal text-gray-700 dark:text-gray-400">Alamat</p>
-                        <h5 class="mb-2 text-md font-semibold tracking-tight text-gray-900 dark:text-white">{{$user->alamat}}
+                        <h5 class="mb-2 text-md font-semibold tracking-tight text-gray-900 dark:text-white">{{$user->alamat ? $user->alamat : '-'}}
                         </h5>
                     </div>
                 </div>
