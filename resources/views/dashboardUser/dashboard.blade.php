38ca4db69fe5fc9a2398b9b1cd1c4a7be3a318f0
prasta <prasta72@gmail.com>
2023-07-12 16:47:54 +0800

    4

--- resources/views/dashboardUser/dashboard.blade.php
+++ resources/views/dashboardUser/dashboard.blade.php
@@ -111,7 +111,7 @@
                                 </a>
                             </div>
                             @if ($latest_quis)
-                                <a href="{{$latest_quis ?  $latest_quis->link : '#'}}">
+                                <a href="{{$latest_quis ?  $latest_quis->link : '#'}}" target="blank">
                                     <button type="submit"
                                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Lihat</button>
                                 </a>
