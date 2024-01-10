d7cb2aceb29fb3e532f96e11bcdcc7d72abbf958
prasta <prasta72@gmail.com>
2024-01-10 21:45:53 +0800

    revisi 3

--- resources/views/dashboardAdmin/laporan/nilai.blade.php
+++ resources/views/dashboardAdmin/laporan/nilai.blade.php
@@ -15,7 +15,7 @@
                     <div class="pb-2">
                         <label for="nilai"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nilai</label>
-                        <input type="number" value="{{$nilai ? $nilai->nilai : ''}}" name="nilai" id="nilai"
+                        <input type="number" min="1" max="100" value="{{$nilai ? $nilai->nilai : ''}}" name="nilai" id="nilai"
                             class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Nilai" required="">
                     </div>
@@ -29,7 +29,7 @@
                             <button type="submit"
                                 class="mx-auto text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5  mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">Simpan</button>
                         </a>
-                        <a href="{{ url('/admin/laporan') }}">
+                        <a href="{{ url('/admin/daftar/laporan') }}">
                             <button type="button"
                                 class="mx-auto text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5  mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">Cancel</button>
                         </a>
